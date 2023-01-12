964de80d61f6d64bafc9524075795e5cf02388b4
covanalytics <tsink@covingtonky.gov>
2023-01-12 14:29:09 -0500

    initial commit

---
+++ R/covdata_color.R
@@ -0,0 +1,27 @@
+
+
+#' covdata_color
+#'
+#' @description  This function allows you to access the HEX color code
+#' options in 'covington_colors' by name in applying color or fill ggplot aesthetics.
+#' It is best to access this variable to apply color to the data
+#' components of the graphics plot (i.e., points, bars, lines, etc.)
+#' when only one color is needed.
+#' @param ... Accepts the color name in 'covington_colors'
+#' @keywords covdata_color
+#' @export
+#' @examples
+#' ggplot +
+#' geom_bar(stat = "identity",
+#'         fill = covdata_color("Covington.Blue"))
+
+
+#` function to assign color from 'covdata_colors' vector to geoms in graph
+covdata_color <<- function(...) {
+  cols <- c(...)
+  if (is.null(cols)) {
+    return (covington_colors)
+
+  } else
+    covington_colors[cols]
+}
---
+++ R/covington_colors.R
@@ -0,0 +1,35 @@
+
+#' covington_colors
+#'
+#' @description  covington_colors is a character string that contains the
+#' Pantones and Neutral branding colors, and the original Covington
+#' Blue (Covington.OG.Blue).
+#' @keywords covington_colors
+#' @export
+#' @examples
+#' covington_colors <<- c("Covington.Blue" = "#0047ba",
+#'                       "Covington.Navy" = "#211261",
+#'                       "Covington.Dark.Blue" = "#000b8c",
+#'                       "Covington.Light.Blue" = "#00c1de",
+#'                       "Covington.Green" = "#38d430",
+#'                       "Covington.Red" = "#ea0029",
+#'                       "Covington.Yellow" = "#ffc600",
+#'                       "Covington.Black" = "#000000",
+#'                       "Covington.Gray" = "#878787",
+#'                       "Covington.Light.Gray" = "#cccccc",
+#'                       "Covington.Plain" = "#ffffff",
+#'                       "Covington.OG.Blue" = "#46b5d2")
+
+covington_colors <<- c("Covington.Blue" = "#0047ba",
+                       "Covington.Navy" = "#211261",
+                       "Covington.Dark.Blue" = "#000b8c",
+                       "Covington.Light.Blue" = "#00c1de",
+                       "Covington.Green" = "#38d430",
+                       "Covington.Red" = "#ea0029",
+                       "Covington.Yellow" = "#ffc600",
+                       "Covington.Black" = "#000000",
+                       "Covington.Gray" = "#878787",
+                       "Covington.Light.Gray" = "#cccccc",
+                       "Covington.Plain" = "#ffffff",
+                       "Covington.OG.Blue" = "#46b5d2")
+
