2cd4b05b0981ac423d737bc8d0a8059f4cbe6f14
covanalytics <tsink@covingtonky.gov>
2023-01-27 13:03:31 -0500

    added

--- R/covdata_color_d.R
+++ R/covdata_color_d.R
@@ -3,7 +3,6 @@
 #'
 #' @description  This function is used to apply a color aesthetic to a discrete variable.
 #' Color palette options come from 'covdata_palette_colors'.
-#' options in 'covington_colors' by name in applying color or fill ggplot aesthetics.
 #' @param name is the name of the color palette from 'covdata_palette_colors'.
 #' @param reverse is used to change the default order of colors contained in the selected palette.
 #' @keywords covdata_color_d
---
+++ R/covdata_fill_d.R
@@ -0,0 +1,24 @@
+
+#' covdata_color_d
+#'
+#' @description  This function is used to apply a fill aesthetic to a discrete variable.
+#' Color palette options come from 'covdata_palette_colors'.
+#' @param name is the name of the color palette from 'covdata_palette_colors'.
+#' @param reverse is used to change the default order of colors contained in the selected palette.
+#' @keywords covdata_fill_d
+#'
+#' @examples
+#' cov_graph +
+#' geom_bar(stat = "identity", aes(fill = Group))+
+#' covdata_fill_d("Pantones")
+#'
+#' @export
+covdata_fill_d <<- function(name, reverse = FALSE){
+  if(reverse) {
+    ggplot2::scale_fill_manual(values = covdata_palettes(name,
+                                                         type = "discrete",
+                                                         reverse = TRUE))
+  }else
+    ggplot2::scale_fill_manual(values = covdata_palettes(name,
+                                                         type = "discrete"))
+}
