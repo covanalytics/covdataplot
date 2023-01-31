2446cf2087fd561e3a15d61a9356443822e0712e
covanalytics <tsink@covingtonky.gov>
2023-01-31 11:28:50 -0500

    change to all lower case

--- R/covington_colors.R
+++ R/covington_colors.R
@@ -6,29 +6,29 @@
 #' Blue (Covington.OG.Blue).
 #' @keywords covington_colors
 #' @examples
-#' covington_colors <<- c("Covington.Blue" = "#0047ba",
-#'                       "Covington.Navy" = "#211261",
-#'                       "Covington.Dark.Blue" = "#000b8c",
-#'                       "Covington.Light.Blue" = "#00c1de",
-#'                       "Covington.Green" = "#38d430",
-#'                       "Covington.Red" = "#ea0029",
-#'                       "Covington.Yellow" = "#ffc600",
-#'                       "Covington.Black" = "#000000",
-#'                       "Covington.Gray" = "#878787",
-#'                       "Covington.Light.Gray" = "#cccccc",
-#'                       "Covington.Plain" = "#ffffff",
-#'                       "Covington.OG.Blue" = "#46b5d2")
+#' covington_colors <<- c("covington.blue" = "#0047ba",
+#'                       "covington.navy" = "#211261",
+#'                       "covington.dark.blue" = "#000b8c",
+#'                       "covington.light.blue" = "#00c1de",
+#'                       "covington.green" = "#38d430",
+#'                       "covington.red" = "#ea0029",
+#'                       "covington.yellow" = "#ffc600",
+#'                       "covington.black" = "#000000",
+#'                       "covington.gray" = "#878787",
+#'                       "covington.light.gray" = "#cccccc",
+#'                       "covington.plain" = "#ffffff",
+#'                       "covington.og.blue" = "#46b5d2")
 #' @export
-covington_colors <<- c("Covington.Blue" = "#0047ba",
-                       "Covington.Navy" = "#211261",
-                       "Covington.Dark.Blue" = "#000b8c",
-                       "Covington.Light.Blue" = "#00c1de",
-                       "Covington.Green" = "#38d430",
-                       "Covington.Red" = "#ea0029",
-                       "Covington.Yellow" = "#ffc600",
-                       "Covington.Black" = "#000000",
-                       "Covington.Gray" = "#878787",
-                       "Covington.Light.Gray" = "#cccccc",
-                       "Covington.Plain" = "#ffffff",
-                       "Covington.OG.Blue" = "#46b5d2")
+covington_colors <<- c("covington.blue" = "#0047ba",
+                       "covington.navy" = "#211261",
+                       "covington.dark.blue" = "#000b8c",
+                       "covington.light.blue" = "#00c1de",
+                       "covington.green" = "#38d430",
+                       "covington.red" = "#ea0029",
+                       "covington.yellow" = "#ffc600",
+                       "covington.black" = "#000000",
+                       "covington.gray" = "#878787",
+                       "covington.light.gray" = "#cccccc",
+                       "covington.plain" = "#ffffff",
+                       "covington.og.blue" = "#46b5d2")
 
