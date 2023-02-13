b267082c7d61df729708cfade90f3834d1d6ad1e
covanalytics <tsink@covingtonky.gov>
2023-02-13 12:10:24 -0500

    covington_logos

---
+++ R/covington_logos.R
@@ -0,0 +1,18 @@
+
+#' covington_logos
+#'
+#' @description  covington_logos is a character string that contains the
+#' names of the four covington logos that are permitted with the CovData graphics
+#' style.  Each of the '.png' logo files must be available in the working directory
+#' @keywords covington_logos
+#' @examples
+#' covington_logos <<- c("cov.seal.blue"="cov.seal.BLUE.png",
+#'                       "cov.seal.white"="cov.seal.WHITE.png",
+#'                       "cov.logo.blue"="cov.logo.BLUEtm.png",
+#'                       "cov.logo.white"="cov.logo.WHITEtm.png")
+
+#' @export
+covington_logos <<- c("cov.seal.blue"="cov.seal.BLUE.png",
+                      "cov.seal.white"="cov.seal.WHITE.png",
+                      "cov.logo.blue"="cov.logo.BLUEtm.png",
+                      "cov.logo.white"="cov.logo.WHITEtm.png")
